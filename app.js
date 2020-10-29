b8d6fbd94d082155ce87df8fdf4bee6a0da52bff
unknown <jreidmke@gmail.com>
2020-10-29 14:31:58 -0500

    init and import express error for handling 404

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const app = express();
+const ExpressError = require("./expressError");
 
 //before we hit the routes. let's do the functions first.
 
@@ -8,14 +10,56 @@ function mean(arr) {
     }))/arr.length);
 }
 
+app.get('/', (req, res) => {
+    return res.send("THIS IS PIZZA");
+})
+
 function median(arr) {
-
+    return (arr.sort(function(a, b){return a - b})[Math.floor(arr.length/2)]);
 }
 
-function mode(arr) {
-
+function mode(array)
+{
+    if(array.length == 0)
+        return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+        if(modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;
+        if(modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
 }
 
-console.log(mean([1, 2, 3]));
+const arr = [1, 2, 2, 3];
 
-module.exports = { mean };
+console.log(mean(arr));
+console.log(median(arr));
+console.log(mode(arr));
+
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
+  });
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+
+    return res.json({
+      error: err,
+      message: err.message
+    });
+  });
+
+app.listen(3000, function() {
+    console.log("SERVER RUNNING");
+})
